a75bfc1906c413dcfef4156f591c769e860288a1
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-25 09:51:46 +0530

    Add:Welcome message

---
+++ Charge.java
@@ -0,0 +1,8 @@
+public class Charge                                     //Main class
+{
+
+	public static void main(String[] args)          //Main method
+	{
+		System.out.println("Welcome to Charge class");
+	}
+}
